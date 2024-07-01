99f9e1e212e27f6d43f04164e60933c33be194c4
Shane Monastero <smonastero.business@gmail.com>
2024-07-01 18:06:19 -0400

    Updated (7/1)

--- test/fuzz/continueOnRevert/ContinueOnRevertInvariants.t.sol
+++ test/fuzz/continueOnRevert/ContinueOnRevertInvariants.t.sol
@@ -2,10 +2,9 @@
 
 pragma solidity ^0.8.19;
 
-// // Invariants:
-// // protocol must never be insolvent / undercollateralized
-// // users cant create stablecoins with a bad health factor
-// // a user should only be able to be liquidated if they have a bad health factor
+// Invariants:
+// This protocol must never be undercollateralized
+// users will not be able to create stablecoins with a bad "healthfactor"
 
 import { Test } from "forge-std/Test.sol";
 import { StdInvariant } from "forge-std/StdInvariant.sol";
